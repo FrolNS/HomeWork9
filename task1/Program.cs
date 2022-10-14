6339be9a0e304e460107865b6fcd926b2712e2db
frol_cao <nkita_0404@mail.ru>
2022-10-14 18:32:03 +0300

    Seminar 9

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет
+все натуральные числа в промежутке от N до 1.
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+void getNumbers(int n)
+{
+    if (n != 0)
+    {
+        if(n != 1)
+        {
+            Console.Write($"{n}, ");
+            getNumbers(n - 1);
+        }
+        else 
+        {
+            Console.Write($"{n}");
+        }
+    }
+}
+
+Console.Write("Введите число N: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+getNumbers(userNumber);
---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+int getSumOfNumbers(int m, int n)
+{
+    if (m > n)
+    {
+        return 0;
+    }
+    int sum = n;
+    return sum + getSumOfNumbers(m, n - 1);
+}
+
+Console.Write("Введите число M: ");
+int userNumberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int userNumberN = Convert.ToInt32(Console.ReadLine());
+int sum = getSumOfNumbers(userNumberM, userNumberN);
+Console.WriteLine(sum);
+
+
